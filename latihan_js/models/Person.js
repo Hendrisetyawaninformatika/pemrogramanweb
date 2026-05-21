6634b3ebdc3c765148962b1995805d9eadaf83f4
Hendrisetyawaninformatika <setyawanhendri253@gmail.com>
2026-05-21 10:50:33 +0700

    new folder

---
+++ latihan_js/models/Person.js
@@ -0,0 +1,11 @@
+export default class person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    greet () {
+        console.log('Helo, Hendri ${this. name}, umur ${thid.age} tahun');
+    }
+}
